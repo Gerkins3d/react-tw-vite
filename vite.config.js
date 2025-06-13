efa3905b6c223d9adbcaf28cb3405616671a75b4
Герман <gerkins2007@gmail.com>
2025-06-13 23:14:39 +0300

    Fixed work on tw

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(), // Поддержка React
-    tailwindcss(), // Интеграция Tailwind
-  ],
-  server: {
-    port: 3000,
-    open: true,
-  },
+  plugins: [react(), tailwindcss()],
 });
